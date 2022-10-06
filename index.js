ddfd44bae640ec228c7497bd73fddf516b262c77
Cláduio Almeida <cmma89@gmail.com>
2022-10-06 23:36:40 +0100

    refactor

--- index.js
+++ index.js
@@ -1,27 +1,22 @@
-const Discord = require('discord.js');
-const {Client, Intents} = require('discord.js');
-const bot = new Client({intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]});
-//const https = require('https');
+const { Client, Intents } = require('discord.js');
+const bot = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const https = require('https');
 const axios = require('axios').default;
+const keepAlive = require("./server")
 
-
-let UPDATE_INTERVAL;  // Price update interval in milliseconds
-let TICKER;           // Which ticker to pull price for
-let TOKEN_INDEX;      // Discord bot token index to use (in auth.json)
-let ROTATE_PRICE;     // If unpopulated, keep price as $, otherwise rotate between $, Ξ and ₿ every 10 seconds
-
-let priceData;
 let guildMeCache = [];
+const URL = "https://api.dexscreener.io/latest/dex/pairs/avalanche/0x218E154Dc5EF516dBCd352EA8d4a2dCE10d11669";
 
+keepAlive()
 // Ready up activities
 bot.on('ready', () => {
     console.log(`Logged in as ${bot.user.tag}!`);
     bot.user.setActivity(`😀`);
 
     bot.guilds.cache.each(guild => guildMeCache.push(guild.me));
-    //guildMeCache.forEach(guildMe => guildMe.setNickname(`${priceData.ticker} ${showPriceType}${priceData[priceKey].currPrice} ${priceData[priceKey].changeArrow}`));
-    UPDATE_INTERVAL = 300000; //5min
-    //UPDATE_INTERVAL = 3000;
+
+    UPDATE_INTERVAL = 60000; //1min
+
     getPrice();
     setInterval(getPrice, UPDATE_INTERVAL);
 })
@@ -33,47 +28,20 @@ bot.on('guildCreate', guild => {
     console.log(`New server has added the bot! Name: ${guild.name}`);
 });
 
-bot.login(${TOKEN});
+bot.login(process.env['TOKEN']);
 
 function getPrice() {
     //https://docs.dexscreener.com/#undefined
-    let url = "https://api.dexscreener.io/latest/dex/pairs/avalanche/0x218E154Dc5EF516dBCd352EA8d4a2dCE10d11669";
     let json = {};
-    /*  let request = https.get(url, (res) => {
-          if (res.statusCode !== 200) {
-              console.error(`Did not get an OK from the server. Code: ${res.statusCode}`);
-              res.resume();
-             return;
-          } else {
-              let data = '';
-
-              res.on('data', (chunk) => {
-                  data += chunk;
-              });
-          }
-
-
-          res.on('end', () => {
-              console.log('Retrieved all data');
-              json = JSON.parse(data)["pair"];
-              console.log(json);
-              guildMeCache.forEach(guildMe => guildMe.setNickname(`${json["baseToken"]['symbol']}: ${json["priceUsd"]}$ `));
-              bot.user.setActivity(`24h: ${json["priceChange"]['h24']}}%`, {type: "WATCHING"});
-          });
-      })*/
 
     axios.get(url)
         .then(function (res) {
-            //console.log(res);
-            /*    let data = '';
-                data += res.data;*/
-
             if (res.data) {
                 console.log('Retrieved all data');
                 //console.log(data);
                 json = res.data["pair"];
                 console.log(json);
-                guildMeCache.forEach(guildMe => guildMe.setNickname(`${json["baseToken"]['symbol']}: ${json["priceUsd"]}$ `));
+                guildMeCache.forEach(guildMe =>                 guildMe.setNickname(`${json["baseToken"]['symbol']}: ${json["priceUsd"]}$ `));
                 bot.user.setActivity(`24h: ${json["priceChange"]['h24']}}%`, {type: "WATCHING"});
             }
 

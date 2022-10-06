4ce33f07e4d78c6c5382854334cdfa4ee5504203
Cláduio Almeida <cmma89@gmail.com>
2022-10-06 23:54:45 +0100

    port issues

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const server = express()
 server.all("/", (req,res) => {res.send("Bot is Running!")})
 
 function keepAlive() {
-    server.listen(3000, () => {
+    server.listen(process.env.PORT || 5000, () => {
         console.log("Server is ready.")
     })
 }
